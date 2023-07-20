c0d266ba792c816bd3f89c568a347d7bff2ebe51
Zied97 <benhassounzied@gmail.com>
2023-07-20 23:43:38 +0100

    CRUD

---
+++ task1/src/components/TodoForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const TodoForm = () => {
+  return (
+    <form className='TodoForm'>
+      <input type="text" className='todo_input' placeholder='What is the task today ?'> </input>
+
+    </form>
+  )
+}
+
